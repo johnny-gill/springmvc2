26a16c95c69472fa356fbb89ae3cb1a9eb3feeb4
DBS\P0001746 <sg.lim@dbhitek.com>
2022-10-18 15:59:23 +0900

    rss json변환 등

--- src/test/java/hello/thymeleaf/ThymeleafBasicApplicationTests.java
+++ src/test/java/hello/thymeleaf/ThymeleafBasicApplicationTests.java
@@ -1,6 +1,8 @@
 package hello.thymeleaf;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndEntryImpl;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
@@ -12,29 +14,21 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 @SpringBootTest
 class ThymeleafBasicApplicationTests {
 
-	@Test
-	void test() throws IOException, FeedException {
+	static class CustomSyndEntryImpl extends SyndEntryImpl {
 
-		List<MultiValueMap<String, Object>> list = new ArrayList<>();
-		String url = "https://trends.google.com/trends/trendingsearches/daily/rss?geo=KR";
-
-		SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(url)));
-		for (SyndEntry entry : feed.getEntries()) {
-			MultiValueMap<String, Object> multiValueMap = new LinkedMultiValueMap<>();
-			multiValueMap.add("title", entry.getTitle());
-			multiValueMap.add("description", entry.getDescription().getValue());
-			multiValueMap.add("link", entry.getLink());
-			multiValueMap.add("publishedDate", entry.getPublishedDate());
-
-			for (Element element : entry.getForeignMarkup()) {
+		private MultiValueMap<String, Object> multiValueMap = new LinkedMultiValueMap<>();
+		@Override
+		public void setForeignMarkup(List<Element> foreignMarkup) {
+			for (Element element : foreignMarkup) {
 				String name = element.getName();
-
 				if ("news_item".equals(name)) {
 					Map<String, String> childMap = new HashMap<>();
 					for (Element child : element.getChildren()) {
@@ -46,12 +40,40 @@ class ThymeleafBasicApplicationTests {
 					multiValueMap.add(name, value);
 				}
 			}
+		}
+
+		public MultiValueMap<String, Object> getForeignMarkupMap() {
+			return multiValueMap;
+		}
+	}
+
+	@Test
+	void test() throws IOException, FeedException {
+
+		List<MultiValueMap<String, Object>> list = new ArrayList<>();
+
+//		String url = "https://trends.google.com/trends/trendingsearches/daily/rss?geo=JP";
+//		SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(url)));
+
+		URL url = getClass().getClassLoader().getResource("static/test.rss");
+		SyndFeed feed = new SyndFeedInput().build(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
+
+		for (SyndEntry entry : feed.getEntries()) {
+			CustomSyndEntryImpl customEntry = new CustomSyndEntryImpl();
+			customEntry.copyFrom(entry);
+
+			MultiValueMap<String, Object> multiValueMap = new LinkedMultiValueMap<>();
+			multiValueMap.add("title", customEntry.getTitle());
+			multiValueMap.add("description", customEntry.getDescription().getValue());
+			multiValueMap.add("link", customEntry.getLink());
+			multiValueMap.add("pubDate", customEntry.getPublishedDate().toString());
+
+			customEntry.setForeignMarkup(entry.getForeignMarkup());
+			multiValueMap.addAll(customEntry.getForeignMarkupMap());
 			list.add(multiValueMap);
 		}
 
-		list.sort((Comparator.comparing(o -> o.get("publishedDate").toString())));
-		for (MultiValueMap<String, Object> multiValueMap : list) {
-			System.out.println(multiValueMap);
-		}
+		list.sort((Comparator.comparing(o -> o.get("pubDate").toString())));
+		System.out.println(new ObjectMapper().writeValueAsString(list));
 	}
 }
