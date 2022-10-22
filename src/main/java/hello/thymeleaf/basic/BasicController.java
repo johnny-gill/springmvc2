6cf8f6dc09b8a040c571d67ed4c3fa3691b0aed7
johnny-gill <uint777@gmail.com>
2022-10-22 14:27:10 +0900

    조건부 평가

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -109,16 +109,22 @@ public class BasicController {
 
     @GetMapping("/each")
     public String each(Model model) {
-        addUser(model);
+        addUsers(model);
         return "basic/each.html";
     }
 
-    private static void addUser(Model model) {
+    private static void addUsers(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("userA", 10));
         list.add(new User("userB", 20));
         list.add(new User("userC", 30));
 
         model.addAttribute("users", list);
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
     }
 }
